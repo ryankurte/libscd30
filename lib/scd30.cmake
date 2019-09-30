6d5141d7ca145415b95c3e0b8b63c168fe9f7fed
Ryan Kurte <ryankurte@gmail.com>
2019-09-30 16:25:29 +1300

    hacks for IDF

--- lib/scd30.cmake
+++ lib/scd30.cmake
@@ -1,13 +1,6 @@
+# SCD30 library cmake file
 
-# List sources
-set(SCD30_SOURCES
-    ${CMAKE_CURRENT_LIST_DIR}/source/scd30.c
-)
-
-
-# Allow headers to be located
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-# Add the SCD30 library
-add_library(scd30 ${SCD30_SOURCES})
+set(SCD30_SOURCES ${CMAKE_CURRENT_LIST_DIR}/source/scd30.c)
 
